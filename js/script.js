ea97d951dcc5dec46e6f8578896159da58b302ce
raphael <raphaelsantosdesigner@gmail.com>
2020-02-13 14:57:58 -0800

    Create function to fetch the data with new city

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ defaultState = () => {
     const defaultCity = "Vancouver";           
         fetch(`http://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&units=imperial&appid=40056036c1703d275c5175eb77a05d0d`).then(function(response){
             if(response.status == 404) {
-                alert("Hey the city is wrong")
+                alert("Hey the city is wrong") 
                 return;
             }
             response.json().then(function(data){
-               console.log(data);
+              // console.log(data);
             })
             
             }).catch(function(error){
@@ -18,19 +18,26 @@ defaultState = () => {
 //Function to fetch the data when the user click on the buttom
 
 fetchCurrentyData = () => {
-    let currentCity = $("#name").value();
+    event.preventDefault();
+    let currentCity = $("#name").val();
+    console.log(currentCity);
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=imperial&appid=40056036c1703d275c5175eb77a05d0d`;
     fetch(url).then((response) => {
         let status = response.status;
         if(status !== 200) {
             console.log("Please check your response status" + status);
+            return;
         }
+
+        response.json().then((data) => {
+            console.log(data);
+        })
     })
 }
 
 $(document).ready(() => {
     defaultState(); // call the function when the page is reload
     $(".searchBtn").on('click', () => {
-        alert("galo doido")
+        fetchCurrentyData();
     })
 });
